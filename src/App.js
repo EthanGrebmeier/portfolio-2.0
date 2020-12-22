927626236d04e69af33697b520b304d726120e29
Ethan Grebmeier <ethangrebmeier@gmail.com>
2020-12-22 11:53:24 -0700

    Added the majority of the projects to be hosted within the portfolio

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import Navigation from './components/Navigation/Navigation'
 import PortfolioContent from './components/PortfolioContent/PortfolioContent'
 import MobileBurger from './components/MobileBurger/MobileBurger'
+
+import Sorter from './projects/Sorter/App'
+import SayHello from './projects/SayHello/App'
+import Reader from './projects/Reader/App'
+import Guesser from './projects/Guesser/App'
+
+
 import {getColorsObject, getColors} from './components/colors/colors.js'
-
-
 
 function App() {
 
@@ -36,6 +41,11 @@ function App() {
         <Route exact path="/" render={ () => ( <Redirect to="/about" />) } />
         <Route exact path="/:page" render={ ({ match }) => (<PortfolioContent key={match.params.page} setShowMobileNav={setShowMobileNav} page={match.params.page} getColors={getColors} getColorsObject={getColorsObject} colorMode={colorMode} switchColorMode={setColorMode}/>) } />
         <Route path="/projects/:projectName" render={ ({ match }) => (<PortfolioContent key={match.params.projectName} setShowMobileNav={setShowMobileNav} page="project" projectName={match.params.projectName} getColors={getColors} getColorsObject={getColorsObject} colorMode={colorMode} switchColorMode={setColorMode}/>) } />
+
+        <Route exact path="/demo/sorter" component={Sorter} />
+        <Route exact path="/demo/say-hello" component={SayHello} />
+        <Route exact path="/demo/reader" component={Reader} />
+        <Route exact path="/demo/guesser" component={Guesser} />
 
         <span className="connector-container">
 
